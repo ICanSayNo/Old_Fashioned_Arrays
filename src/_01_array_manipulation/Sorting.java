adf4b9646586b58a8e523022e200b64f08843c38
Isaacpants <league@WTS5.attlocal.net>
2018-07-16 19:56:04 -0700

    idek

--- src/_01_array_manipulation/Sorting.java
+++ src/_01_array_manipulation/Sorting.java
@@ -2,4 +2,19 @@ package _01_array_manipulation;
 
 public class Sorting {
 
+	public static String[] sort(String[] testArray)
+    {
+        int n = testArray.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (testArray[j] > testArray[j+1]){
+                    String temp = testArray[j];
+                    testArray[j] = testArray[j+1];
+                    testArray[j+1] = temp;
+                }
+            }
+        }
+        return testArray;
+    }
 }
+
